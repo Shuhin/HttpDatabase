612b5aeaeb65208bf42c8ccf31b58311bdcef259
Syed Adiba Shuhin <adibashuhin@gmail.com>
2019-02-14 12:53:40 +0600

    Add files via upload

--- HttpDatabase.js
+++ HttpDatabase.js
@@ -1,8 +1,8 @@
 var http = require ('http');
-var express = require('express');
-var fs = require('fs');
+//var express = require('express');
+//var fs = require('fs');
 var mysql= require('mysql');
-var app = express();
+//var app = express();
 //app.use(bodyparser.json());
 
 var connection = mysql.createConnection({
@@ -20,8 +20,8 @@ connection.connect(function(error){
   }
 });
 
-
-app.get('/get', function(req, res){
+var  server = http.createServer(function(req, res){
+  if(req.url ==='/get'|| req.url === "/"){
   console.log('request was made: '+ req.url);
     connection.query("SELECT * FROM todo" , function(error,rows, fields){
           if(!!error) {
@@ -29,36 +29,33 @@ app.get('/get', function(req, res){
           } else {
             console.log('Successful query');
             console.log(rows);
-            res.send(JSON.stringify(rows));
+            res.end(JSON.stringify(rows));
           }
         });
-      });
-
- app.get('/post',function(req, res){
-   console.log('request was made: '+ req.url);
+      } else if(req.url ==="/post"){
+            console.log('request was made: '+ req.url);
             connection.query("INSERT INTO todo (ItemNo , Item) VALUES (1, 'Buy Flower')" , function(error,rows, fields){
                if(!!error) {
                  console.log ('Error in the query');
                } else {
                  console.log('Successful query');
                  console.log(rows);
-                   res.send(JSON.stringify(rows));
+                   res.end(JSON.stringify(rows));
                }
              });
-           });
-
-app.get('/Delete', function(req, res){
-  connection.query("DELETE FROM todo WHERE Item='Buy Flower'", function(error,rows, fields){
-     if(!!error) {
-       console.log ('Error in the query');
-     } else {
-       console.log('Successful query');
-       console.log(rows);
-         res.send(JSON.stringify(rows));
-     }
+           } else if(req.url ==="/delete"){
+           connection.query("DELETE FROM todo WHERE Item='Buy Flower'", function(error,rows, fields){
+               if(!!error) {
+                 console.log ('Error in the query');
+               } else {
+                 console.log('Successful query');
+                 console.log(rows);
+                   res.end(JSON.stringify(rows));
+               }
    });
- });
+ }
+});
 
-app.listen(3000);
+server.listen(3000);
 
 console.log("Now listening to port 3000");
