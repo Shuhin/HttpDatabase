dd3c53d519e35403c6044485668427d8a455f163
Syed Adiba Shuhin <adibashuhin@gmail.com>
2019-02-14 17:13:38 +0600

    Add files via upload

--- HttpDatabase.js
+++ HttpDatabase.js
@@ -1,7 +1,7 @@
-var http = require ('http');
+var http = require('http');
 //var express = require('express');
 //var fs = require('fs');
-var mysql= require('mysql');
+var mysql = require('mysql');
 //var app = express();
 //app.use(bodyparser.json());
 
@@ -12,48 +12,48 @@ var connection = mysql.createConnection({
   database: 'todoDB'
 });
 
-connection.connect(function(error){
-  if(!!error) {
+connection.connect(function(error) {
+  if (!!error) {
     console.log('Error');
   } else {
     console.log('Connected');
   }
 });
 
-var  server = http.createServer(function(req, res){
-  if(req.url ==='/get'|| req.url === "/"){
-  console.log('request was made: '+ req.url);
-    connection.query("SELECT * FROM todo" , function(error,rows, fields){
-          if(!!error) {
-            console.log ('Error in the query');
-          } else {
-            console.log('Successful query');
-            console.log(rows);
-            res.end(JSON.stringify(rows));
-          }
-        });
-      } else if(req.url ==="/post"){
-            console.log('request was made: '+ req.url);
-            connection.query("INSERT INTO todo (ItemNo , Item) VALUES (1, 'Buy Flower')" , function(error,rows, fields){
-               if(!!error) {
-                 console.log ('Error in the query');
-               } else {
-                 console.log('Successful query');
-                 console.log(rows);
-                   res.end(JSON.stringify(rows));
-               }
-             });
-           } else if(req.url ==="/delete"){
-           connection.query("DELETE FROM todo WHERE Item='Buy Flower'", function(error,rows, fields){
-               if(!!error) {
-                 console.log ('Error in the query');
-               } else {
-                 console.log('Successful query');
-                 console.log(rows);
-                   res.end(JSON.stringify(rows));
-               }
-   });
- }
+var server = http.createServer(function(req, res) {
+  if (req.url === '/get' || req.url === "/") {
+    console.log('request was made: ' + req.url);
+    connection.query("SELECT * FROM todo", function(error, rows, fields) {
+      if (!!error) {
+        console.log('Error in the query');
+      } else {
+        console.log('Successful query');
+        console.log(rows);
+        res.end(JSON.stringify(rows));
+      }
+    });
+  } else if (req.url === "/post") {
+    console.log('request was made: ' + req.url);
+    connection.query("INSERT INTO todo (ItemNo , Item) VALUES (1, 'Buy Flower')", function(error, rows, fields) {
+      if (!!error) {
+        console.log('Error in the query');
+      } else {
+        console.log('Successful query');
+        console.log(rows);
+        res.end(JSON.stringify(rows));
+      }
+    });
+  } else if (req.url === "/delete") {
+    connection.query("DELETE FROM todo WHERE Item='Buy Flower'", function(error, rows, fields) {
+      if (!!error) {
+        console.log('Error in the query');
+      } else {
+        console.log('Successful query');
+        console.log(rows);
+        res.end(JSON.stringify(rows));
+      }
+    });
+  }
 });
 
 server.listen(3000);
