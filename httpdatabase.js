8e76ec364ff983526395667f4b52a636b37c3854
Syed Adiba Shuhin <adibashuhin@gmail.com>
2019-02-20 12:24:30 +0600

    Add files via upload
    Error handled and connection problem solved.

---
+++ httpdatabase.js
@@ -0,0 +1,64 @@
+let http = require('http');
+let mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'DB'
+});
+
+let server = http.createServer(function(req, res) {
+
+  if (req.url === '/get' || req.url === "/") {
+
+    console.log('request was made: ' + req.url);
+    connection.query("SELECT * FROM DB", function(error, rows) {
+      if (error) {
+        console.log('Error in the query');
+        res.writeHead(400, {'content-Type': 'text/plain'});
+        res.end('400 Bad Request, Please check you query again');
+      } else {
+        res.writeHead(200, {'content-type': 'application/json'});
+        console.log('Successful query');
+        console.log(rows);
+        res.end(JSON.stringify(rows));
+
+      }
+    });
+
+  } else if (req.url === "/post") {
+    console.log('request was made: ' + req.url);
+    connection.query("INSERT INTO `DB`(`Item No`, `Item`) VALUES (2 , 'Buy chocolate')", function(error, rows) {
+      if (error) {
+        console.log('Error in the query');
+        res.writeHead(400, { 'content-Type': 'text/plain'});
+        res.end('400 Bad Request, Please check you query again');
+        server.close();
+      } else {
+        res.writeHead(200, {'content-type': 'application/json'});
+        console.log('Successful query');
+        console.log(rows);
+        res.end(JSON.stringify(rows));
+      }
+    });
+
+  } else if (req.url === "/delete") {
+    connection.query("DELETE FROM DB WHERE Item ='Buy chocolate'", function(error, rows) {
+      if (error) {
+      console.log('Error in the query');
+      res.writeHead(400, { 'content-Type': 'text/plain'});
+      res.end('400 Bad Request, Please check you query again');
+
+      } else {
+        console.log('Successful query');
+        console.log(rows);
+        res.end(JSON.stringify(rows));
+      }
+    });
+  }
+});
+
+server.listen(3000);
+
+console.log("Now listening to port 3000");
