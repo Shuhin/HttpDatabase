2d656750720443f2c4553f4eee91d67bc3e28eb4
Syed Adiba Shuhin <adibashuhin@gmail.com>
2019-02-26 12:46:33 +0600

    Add files via upload

--- middleware.js
+++ middleware.js
@@ -4,6 +4,8 @@ let mysql = require('mysql');
 
 let app = express();
 
+let errorhandler = require('errorhandler');
+
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -11,26 +13,34 @@ var connection = mysql.createConnection({
   database: 'DB'
 });
 
-app.use(function check(req,res){
+function errorNotification (err, str, req) {
+  let title = 'Error in in the query '
+
+  notifier.notify({
+    title: title,
+    message: str
+  })
+}
+
+app.use(function check(req, res, next){
   connection.connect(function(error) {
     if (error) {
       res.writeHead(500, {'content-Type': 'text/plain'});
       res.end('500 Server Error, Something went wrong with the connection');
-      console.log('Due to Connection problem server is forcibly closed');
+      console.log('Connection problem ');
     } else {
       console.log("Database Connection OK")
+        next();
     }
   });
-});
 
+});
 
 app.get('/', function(req, res) {
       console.log('request was made: ' + req.url);
       connection.query("SELECT * FROM DB", function(error, rows) {
         if (error){
-              console.log('Error in the query');
-              res.writeHead(400, {'content-Type': 'text/plain'});
-              res.end('400 Bad Request, Please check your query again');
+              app.use(errorhandler({log: errorNotification}))
         } else {
           res.writeHead(200, {'content-type': 'application/json'});
           console.log('Successful query');
@@ -43,10 +53,8 @@ app.get('/', function(req, res) {
 app.get('/post', function(req, res) {
   console.log('request was made: ' + req.url);
   connection.query("INSERT INTO `DB`(`Item No`, `Item`) VALUES (2 , 'Buy chocolate')", function(error, rows) {
-  if (error){
-          console.log('Error in the query');
-          res.writeHead(400, {'content-Type': 'text/plain'});
-          res.end('400 Bad Request, Please check your query again');
+    if (error){
+          app.use(errorhandler({log: errorNotification}))
     } else {
       res.writeHead(200, {
         'content-type': 'application/json',
@@ -57,17 +65,13 @@ app.get('/post', function(req, res) {
       res.end(JSON.stringify(rows) + "\n" + 'Successfully inserted');
     }
   });
-  });
+});
 
 
 app.get('/delete', function(req, res) {
      connection.query("DELETE FROM DB WHERE Item ='Buy chocolate'", function(error, rows) {
-       if (error) {
-             console.log('Error in the query');
-             res.writeHead(400, {
-               'content-Type': 'text/plain'
-             });
-             res.end('400 Bad Request, Please check your query again');
+       if (error){
+             app.use(errorhandler({log: errorNotification}))
        } else {
          console.log('Successful query');
          console.log(rows);
